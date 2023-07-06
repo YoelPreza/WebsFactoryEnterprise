cf5bb1906c7f6994375f3e32794cf1be8ff22686
Gustavo Morales <gusanmore@gmail.com>
2023-07-06 14:14:15 -0600

    footer and styles in navbar

---
+++ src/components/footerElement.js
@@ -0,0 +1,42 @@
+import { LitElement, html, css } from 'lit';
+import { TWStyles } from '../twlit';
+
+export class FooterELement extends LitElement {
+    static styles = [ TWStyles,
+        css`
+            :host {
+                display: block;
+            }
+        `
+    ];
+
+    render() {
+        return html`
+            <footer class="bg-red-500 sm:h-max text-white text-xl relative bottom-0 py-3 flex flex-col md:flex-row">
+                
+                <div class="main-networks flex flex-col 
+                    md:justify-between  items-center md:items-center md:w-1/2 md:flex-col
+                    ">
+                    <div class="flex items-center  md:w-full p-2 justify-center">
+                        <img class="h-10 mx-3 hover:animate-bounce cursor-pointer" src="src/assets/envelope-solid.svg" />
+                        <p class="mx-3">thewebsfactory@website.com</p>
+                    </div>
+                    <div class="flex justify-center items-center w-max md:w-full md:justify-self-start lg:flex-row ">
+                        <img class="h-10 mx-3 hover:animate-bounce cursor-pointer" src="src/assets/whatsapp.svg" />
+                        <div class="flex">
+                            <p class="mx-3">5532900759</p>
+                            <p class="mx-3">5532900759</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="secundary-networks w-full bg-red-500 flex flex-row p-5 items-center justify-center md:w-1/2 ">
+                    <img class="h-10 mx-5 hover:animate-bounce cursor-pointer" src="src/assets/facebook.svg" />
+                    <img class="h-10 mx-5 hover:animate-bounce cursor-pointer" src="src/assets/instagram.svg" />
+                    <img class="h-10 mx-5 hover:animate-bounce cursor-pointer" src="src/assets/tiktok.svg" />
+                </div>
+            </footer>
+        `;
+    }
+}
+customElements.define('footer-element', FooterELement);
--- src/components/navElement.js
+++ src/components/navElement.js
@@ -32,7 +32,7 @@ export class NavELement extends LitElement {
     }
 
     toggleMEnu(e){
-        this.shadowRoot.querySelector('.links').classList.toggle('-translate-x-[120%]')
+        this.shadowRoot.querySelector('.links').classList.toggle('translate-x-0')
     }
 
     scrollThis(e) {
@@ -44,10 +44,10 @@ export class NavELement extends LitElement {
 
     render() {
         return html`
-            <nav class="text-pink-700 bg-black rounded-lg fixed top-0 w-11/12 left-1/2 -translate-x-1/2 z-50 flex justify-between items-center p-2 px-5">
+            <nav class="text-white bg-black rounded-lg fixed top-0 w-full sm:w-11/12 left-1/2 -translate-x-1/2 z-50 flex justify-between items-center p-2 px-5">
                 <ul class="flex items-center">
                     <img class="mx-3 border h-10 w-10 p-2 rounded-full" src="src/images/logo.png" />
-                    <li class="md:text-white font-bold rounded-xl">${this.brandName}</li>
+                    <li class="sm:hidden md:flex md:text-white font-bold rounded-xl">${this.brandName}</li>
                 </ul>
                 <ul class="links absolute top-full left-0    w-full rounded-lg text-center -translate-x-[120%] transition
                 sm:relative sm:-translate-x-0 sm:flex-row sm:flex sm:justify-end sm:w-max 
