fe6b27f9505fd1ec0d9e6be9cd1fc537ba8766f0
YoelPreza <preza_95@hotmail.com>
2023-07-07 09:58:54 -0600

    servicios listo con estilos mobile

--- src/components/inicioElement.js
+++ src/components/inicioElement.js
@@ -2,45 +2,44 @@ import { LitElement, html, css } from 'lit';
 import {TWStyles} from '../twlit'
 
 export class InicioELement extends LitElement {
-    static styles = [ TWStyles,
-        css`
-            :host {
-                display: block;
-            }
+static styles = [ TWStyles,
+css`
+:host {
+display: block;
+}
 
-            
 
-              
-              
-        `
-    ];
 
-    render() {
-        return html`
-        <main  class="flex flex-col md:flex-row">
 
-        <div class="lg:w-1/2 p-10 w-full h-screen bg-[#9ec59f] flex flex-col justify-center items-center">
-          <div class="typewriter">
-          <h1 class=" text-white m-0 text-5xl text-center font-bold font-sans">
-          ¡ Destaca entre la competencia !
-          </h1>
-          </div>
-          
-          <p class="text-black text-3xl text-center lg:w-1/2 md:w-full w-11/12 my-10">
-          No dejes que tu negocio se quede atrás en el mundo digital 
-          </p>
-          <p class="text-black text-3xl text-center lg:w-1/2 md:w-full w-11/12 mb-10">
-          Creamos páginas web modernas rápidas y fáciles de usar
-          </p>
-          <button href="#" class="bg-black p-4 rounded-full text-white capitalize font-bold">explorar</button>
-        </div>
 
-        <div class="lg:w-1/2 w-full h-screen flex bg-[#9ec59f] justify-center items-center">
-            <img class="rounded-xl " src="./src/images/banner-hero.gif" />
-        </div>
+`
+];
 
-      </main>
-        `;
-    }
+render() {
+return html`
+<main class="flex flex-col md:flex-row">
+
+    <div
+        class="lg:w-1/2 p-10 w-full h-screen  bg-[#9ec59f] flex flex-col justify-center items-center">
+        <h1 class=" text-white m-0 text-5xl text-center font-bold font-sans">
+            ¡ Destaca entre la competencia !
+        </h1>
+
+        <p class="text-black text-3xl text-center lg:w-1/2 md:w-full w-11/12 my-10">
+            No dejes que tu negocio se quede atrás en el mundo digital
+        </p>
+        <p class="text-black text-3xl text-center lg:w-1/2 md:w-full w-11/12 mb-10">
+            Creamos páginas web modernas rápidas y fáciles de usar
+        </p>
+        <button href="#" class="bg-black p-4 rounded-full text-white capitalize font-bold">explorar</button>
+    </div>
+
+    <div class="lg:w-1/2  w-full h-screen flex bg-[#9ec59f] justify-center items-center">
+        <img class="lg:h-full" src="./src/images/banner-hero.gif" />
+    </div>
+
+</main>
+`;
+}
 }
 customElements.define('inicio-element', InicioELement);
--- src/components/servicesElement.js
+++ src/components/servicesElement.js
@@ -1,44 +1,38 @@
 import { LitElement, html, css } from 'lit';
 import {TWStyles} from '../twlit'
+import { servicios } from '../data/servicios';
 
 export class ServicesELement extends LitElement {
-    static styles = [TWStyles,
-        css`
-            :host {
-                display: block;
-            }
-        `
-    ];
+static styles = [TWStyles,
+css`
+:host {
+display: block;
+}
+`
+];
 
-    render() {
-        return html`
+render() {
+return html`
 
-<main class="flex flex-col items-center justify-center bg-[#9ec59f] min-h-screen">
-        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 p-10 place-items-center text-center text-slate-800 text-xl font-semibold">
-          <div class="flex flex-col items-center my-10">
-            <img width="200" class="mx-auto flex" src="src/images/desk_lamp.png" />
-            <h3 class="text-white">Actividad 1</h3>
-            <p class="w-1/2">Primera Actividad de la empresa, algo que la empresa haga como actividad principal</p>  
-          </div>
-          <div class="flex flex-col items-center my-10">
-            <img width="200" src="src/images/laptop-icon.png" />
-            <h3 class="text-white">Actividad 2</h3>
-            <p class="w-1/2 my-2">Primera Actividad de la empresa, algo que la empresa haga como actividad secundaria</p>  
-          </div>
-          <div class="flex flex-col items-center my-10">
-            <img width="200" src="src/images/student_man.png" />
-            <h3 class="text-white">Actividad 3</h3>
-            <p class="w-1/2">Primera Actividad de la empresa, algo que la empresa haga como actividad no muy comun</p>  
-          </div>
-        </div>
-        
-        <div class="flex my-10">
-           <!-- <a href="#" class="bg-green-500 p-4 rounded-full text-white capitalize font-bold">aprender mas</a> -->
-        </div>
+<main class="flex flex-col  items-center justify-center bg-[#9ec59f] min-h-screen
 
-      </main>
-        
-        `;
-    }
+lg:flex-row
+">
+  ${servicios.map(servicio=>html`
+  <div class="grid grid-cols-1 p-10 place-items-center text-center text-slate-800 text-xl font-semibold
+   md:w-[500px]
+   lg:w-[500px] lg:h-[500px] lg:p-0" >
+    <div class="flex flex-col items-center my-10">
+      <img class="mx-auto w-52" src=${servicio.img} alt="icon">
+      <h3 class="text-white text-3xl py-2">${servicio.name}</h3>
+      <p class="w-3/4">${servicio.descripcion}</p>
+    </div>
+  </div>
+`)}
+
+</main>
+
+`;
+}
 }
 customElements.define('servicios-element', ServicesELement);
